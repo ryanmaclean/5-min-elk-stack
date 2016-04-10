b77db99300926571015d797af9f8606b36eddb57
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-10 15:00:58 -0700

    Adding more helper scripts and updating docker-elk to most recent version

---
+++ tools/clone_then_run_elk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone --recursive https://github.com/ryanmaclean/5-minute-belk-stack.git
+cd 5-minute-belk-stack/docker-elk
+docker-compose up
---
+++ tools/start-docker-elk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/ryanmaclean/docker-elk
+cd docker-elk
+docker-compose up
