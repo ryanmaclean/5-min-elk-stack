65a6a8bcbe69dd8cee280ed12ff130558e51e7f3
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-10 16:27:26 -0700

    Reverting to TopBeat stable, adding service starts

--- tools/install_packetbeat.sh
+++ tools/install_packetbeat.sh
@@ -2,3 +2,4 @@
 sudo apt-get install libpcap0.8
 curl -L -O https://download.elastic.co/beats/packetbeat/packetbeat_1.2.1_amd64.deb
 sudo dpkg -i packetbeat_1.2.1_amd64.deb
+sudo /etc/init.d/packetbeat start
--- tools/install_topbeat.sh
+++ tools/install_topbeat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -L -O https://download.elastic.co/beats/topbeat/topbeat_5.0.0-alpha1_amd64.deb
-sudo dpkg -i topbeat_5.0.0-alpha1_amd64.deb
+curl -L -O https://download.elastic.co/beats/topbeat/topbeat_1.2.1_amd64.deb
+sudo dpkg -i topbeat_1.2.1_amd64.deb
+sudo /etc/init.d/topbeat start
