c00ddf856a755248855ad5341274e40d5443ddad
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-10 16:11:04 -0700

    Updated scripts, more pink needed

--- tools/clone_then_run_elk.sh
+++ tools/clone_then_run_elk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git clone --recursive https://github.com/ryanmaclean/5-minute-belk-stack.git
-cd 5-minute-belk-stack/docker-elk
-docker-compose up
+docker-compose up -d 5-minute-belk-stack/docker-elk/docker-compose.yml
+cd /5-minute-belk-stack/beats-dashboards/
+./load.sh -url "http://localhost:9200"
+cd ~
---
+++ tools/install_topbeat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -O https://download.elastic.co/beats/topbeat/topbeat_5.0.0-alpha1_amd64.deb
+sudo dpkg -i topbeat_5.0.0-alpha1_amd64.deb
