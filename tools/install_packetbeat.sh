ed432ff1cf0dea1eea7613c02c734d1b0a1e6e7f
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-10 21:03:33 -0700

    Updated minimum zookeeper instances and added template for packetbeat

--- tools/install_packetbeat.sh
+++ tools/install_packetbeat.sh
@@ -2,4 +2,5 @@
 sudo apt-get install libpcap0.8
 curl -L -O https://download.elastic.co/beats/packetbeat/packetbeat_1.2.1_amd64.deb
 sudo dpkg -i packetbeat_1.2.1_amd64.deb
+curl -XPUT 'http://localhost:9200/_template/packetbeat' -d@/etc/packetbeat/packetbeat.template.json
 sudo /etc/init.d/packetbeat start
