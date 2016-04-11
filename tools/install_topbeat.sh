c43cb5a496c5d75fed30898f5f479078ff4ad3a5
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-10 20:20:05 -0700

    Added RPM steps in comments just in case

--- tools/install_topbeat.sh
+++ tools/install_topbeat.sh
@@ -2,3 +2,9 @@
 curl -L -O https://download.elastic.co/beats/topbeat/topbeat_1.2.1_amd64.deb
 sudo dpkg -i topbeat_1.2.1_amd64.deb
 sudo /etc/init.d/topbeat start
+
+# RPM-based Distros
+#
+#curl -L -O https://download.elastic.co/beats/topbeat/topbeat-1.2.1-x86_64.rpm
+#sudo rpm -vi topbeat-1.2.1-x86_64.rpm
+#sudo /etc/init.d/topbeat start
