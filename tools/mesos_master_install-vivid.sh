ff3bf698b7aa80ee59c909ccc0541900b084173a
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-10 23:09:19 -0700

    Fixing up the madness

--- tools/mesos_master_install-vivid.sh
+++ tools/mesos_master_install-vivid.sh
@@ -7,15 +7,6 @@ DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
 
-# Specific to Xenial Beta 2 (Let's Pretend!)
-#echo "deb http://repos.mesosphere.io/ubuntu vivid main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
-
-# For Older Systems, Install the Java 8 Runtime
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo apt-get -y update
-#sudo apt-get install -y oracle-java8-installer
-#sudo apt-get install -y oracle-java8-set-default
-
 # Run Your Update (Or You Shall Not Pass!)
 sudo apt-get -y update
 
